4e5d4ebd2b3361ca3392c9556ec4388c7bc519cf
MsKim <myclath@snu.ac.kr>
2023-05-04 03:13:44 +0900

    commit...

--- Project1/page.h
+++ Project1/page.h
@@ -1,6 +1,11 @@
 class Page {
     public:
-
+    int getX() { return x;}
+    int getY() { return y; }
+    int getW() { return width; }
+    int getH() { return height; }
+    int getid() { return id; }
+    int getcont() { return content; }
     private:
         int x, y; // position of the page on the board
         int width, height; // width and height of the page 
