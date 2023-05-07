10394f425b1a41adfb86b9dc31a136abd1a5327c
MsKim <myclath@snu.ac.kr>
2023-05-08 00:29:26 +0900

    .....

--- Project1/board.h
+++ Project1/board.h
@@ -1,45 +1,52 @@
 #include <fstream>
-
+#include <vector>
 #include "page.h"
 
+using namespace std;
 using std::endl;
-using std::ofstream; 
+using std::ofstream;
 
 class Board {
-    public:
-        Board(int num_jobs, int width, int height, ofstream& output_stream);
-        ~Board();
+public:
 
-        void print_board();
-        void print_job(int job_idx, char job_type, int id);
+    std::vector <Page> p_v;
+    std::vector <Page> make_l;
 
-        //job functions
-        void insert_page(int x, int y, int width, int height, int id, int content);
-        void delete_page(int id);
-        void modify_content(int id, char content);
-        void modify_position(int id, int x, int y);
+    Board(int num_jobs, int width, int height, ofstream& output_stream);
+    ~Board();
 
-        //made functions
-        void Board::add_board();
-        void Board::delete_board(int id);
-    private:
-        vector <Page> p_v;
-        int num_jobs, width, height; 
-        ofstream& output; 
-        char* board; 
+    void print_board();
+    void print_job(int job_idx, char job_type, int id);
+
+    //job functions
+    void insert_page(int x, int y, int width, int height, int id, char content);
+    void delete_page(int id);
+    void modify_content(int id, char content);
+    void modify_position(int id, int x, int y);
+
+    //made functions
+    void add_board();
+    void delete_board(int id);
+    int num_jobs, width, height;
+    ofstream& output;
+    char* board;
+
+    
+private:
+
 };
 
 
-Board::Board(int num_jobs, int width, int height, ofstream& output_stream): output(output_stream) {
+Board::Board(int num_jobs, int width, int height, ofstream& output_stream) : output(output_stream) {
     this->width = width;
     this->height = height;
     this->num_jobs = num_jobs;
 
-    board = new char[width*height];
+    board = new char[width * height];
 
     for (int h = 0; h < height; h++) {
         for (int w = 0; w < width; w++) {
-            board[h*width + w] = ' ';
+            board[h * width + w] = ' ';
         }
     }
 
@@ -47,59 +54,59 @@ Board::Board(int num_jobs, int width, int height, ofstream& output_stream): outp
 
 Board::~Board() {
     delete board;
-    
+
 }
 
 
 void Board::print_board() {
     int h, w;
-    for (w = 0; w < width+2; w++) output << "- ";
+    for (w = 0; w < width + 2; w++) output << "- ";
     output << endl;
-    
+
     for (h = 0; h < height; h++) {
         output << "| ";
         for (w = 0; w < width; w++) {
-            output << board[h*width + w] << " ";
+            output << board[h * width + w] << " ";
         }
         output << "| " << endl;
     }
 
-    for (w = 0; w < width+2; w++) output << "- ";
+    for (w = 0; w < width + 2; w++) output << "- ";
     output << endl;
 }
 
 void Board::print_job(int job_idx, char job_type, int id) {
-    output << ">> (" << job_idx <<") ";
-    switch(job_type) {
-        
-        case 'i':
-            output << "Insert ";
-            break;
-        case 'd':
-            output << "Delete ";
-            break;
-        case 'm':
-            output << "Modify ";
-            break;
+    output << ">> (" << job_idx << ") ";
+    switch (job_type) {
+
+    case 'i':
+        output << "Insert ";
+        break;
+    case 'd':
+        output << "Delete ";
+        break;
+    case 'm':
+        output << "Modify ";
+        break;
     }
 
     output << id << endl;
 }
 
 
-void Board::insert_page(int x, int y, int width, int height, int id, int content) {
+void Board::insert_page(int x, int y, int width, int height, int id, char content) {
     Page p(x, y, width, height, id, content);
 
     for (int i = 0; i < p_v.size(); i++) {
-        for (int j = y; j < y+height; j++) {
-            for (int k = x; k < x+width; k++) {
-                if (board[width * y + x] == p_v[i].getcont()) {
-                    p_v[i].set_vec(p.getid());
+        for (int j = y; j < y + height; j++) {
+            for (int k = x; k < x + width; k++) {
+                if (board[width * j + k] == p_v[i].getcont()) {
+                    p_v[i].above.push_back(p_v[i].id);
                 }
             }
         }
-        sort(p_v[i].begin(), p_v[i].end());
-        unique(p_v[i].begin(), p_v[i].end());
+        sort(p_v[i].above.begin(), p_v[i].above.end());
+        unique(p_v[i].above.begin(), p_v[i].above.end());
         //오름차순
 
         //조건 : 각 페이지에 접근한 다음, 현재 페이지의 아래에 있다면 above 벡터에 하나씩 넣음.
@@ -107,27 +114,27 @@ void Board::insert_page(int x, int y, int width, int height, int id, int content
     }
     p_v.push_back(p);
     //현재 페이지를 그림 -- **
-    for (int j = y; j < y+height; j++) {
-            for (int k = x; k < x+width; k++) {
-                board[width * y + x] = content;
-            }
-            
+    for (int j = y; j < y + height; j++) {
+        for (int k = x; k < x + width; k++) {
+            board[width * (y+j) + k+x] = content;
+        }
+
     }
     print_board();
 }
 
 void Board::delete_page(int id) {
     delete_board(id);
-    for(int i=0; i<p_v.size(); i++) //p_v에서 지운 아이디를 삭제한다.
+    for (int i = 0; i < p_v.size(); i++) //p_v에서 지운 아이디를 삭제한다.
     {
-        if(p_v[i].id == id)
+        if (p_v[i].id == id)
         {
             p_v.erase(p_v.begin() + i);
         }
         break;
     }
     make_l.pop_back(); //맨뒤의 id는 다시 만들게 아니므로 제거
-    for(int i=make_l.size()-1; i>=0; i--)
+    for (int i = make_l.size() - 1; i >= 0; i--)
     {
         insert_page(make_l[i].x, make_l[i].y, make_l[i].width, make_l[i].height, make_l[i].id, make_l[i].content);
         make_l.pop_back();
@@ -137,9 +144,9 @@ void Board::delete_page(int id) {
 void Board::modify_content(int id, char content) {
     int target;
     delete_board(id);
-    for(int i=0; i<p_v.size(); i++) //p_v에서 지운 아이디를 삭제한다.
+    for (int i = 0; i < p_v.size(); i++) //p_v에서 지운 아이디를 삭제한다.
     {
-        if(p_v[i].id == id)
+        if (p_v[i].id == id)
         {
             target = i;
             p_v[i].content = content;
@@ -148,7 +155,7 @@ void Board::modify_content(int id, char content) {
     }
     insert_page(p_v[target].x, p_v[target].y, p_v[target].width, p_v[target].height, p_v[target].id, p_v[target].content);
     make_l.pop_back(); //맨뒤의 id는 다시 만들게 아니므로 제거
-    for(int i=make_l.size()-1; i>=0; i--)
+    for (int i = make_l.size() - 1; i >= 0; i--)
     {
         insert_page(make_l[i].x, make_l[i].y, make_l[i].width, make_l[i].height, make_l[i].id, make_l[i].content);
         make_l.pop_back();
@@ -157,9 +164,9 @@ void Board::modify_content(int id, char content) {
 void Board::modify_position(int id, int x, int y) {
     int target;
     delete_board(id);
-    for(int i=0; i<p_v.size(); i++) //p_v에서 지운 아이디를 삭제한다.
+    for (int i = 0; i < p_v.size(); i++) //p_v에서 지운 아이디를 삭제한다.
     {
-        if(p_v[i].id == id)
+        if (p_v[i].getid() == id)
         {
             target = i;
             p_v[i].x = x;
@@ -167,9 +174,9 @@ void Board::modify_position(int id, int x, int y) {
         }
         break;
     }
-    insert_page(p_v[target].x, p_v[target].y, p_v[target].width, p_v[target].height, p_v[target].id, p_v[target].content);
+    insert_page(p_v[target].getX(), p_v[target].getY(), p_v[target].getW(), p_v[target].getH(), p_v[target].getid(), p_v[target].getcont());
     make_l.pop_back(); //맨뒤의 id는 다시 만들게 아니므로 제거
-    for(int i=make_l.size()-1; i>=0; i--)
+    for (int i = make_l.size() - 1; i >= 0; i--)
     {
         insert_page(make_l[i].x, make_l[i].y, make_l[i].width, make_l[i].height, make_l[i].id, make_l[i].content);
         make_l.pop_back();
@@ -178,14 +185,14 @@ void Board::modify_position(int id, int x, int y) {
 
 void Board::add_board()
 {
-    for(int i=0; i<p_v.size(); i++)
+    for (int i = 0; i < p_v.size(); i++)
     {
-        for (int j = p_v[i].y; j < p_v[i].y+height; j++) {
-            for (int k = p_v[i].x; k < p_v[i].x+width; k++) 
+        for (int j = p_v[i].y; j < p_v[i].y + height; j++) {
+            for (int k = p_v[i].x; k < p_v[i].x + width; k++)
             {
-                board[width * p_v[i].y + p_v[i].x] = p_v[i].content;
+                board[width * (p_v[i].y+j) + p_v[i].x+k] = p_v[i].content;
             }
-            
+
         }
     }
 }
@@ -193,23 +200,22 @@ void Board::add_board()
 
 void Board::delete_board(int id)
 {
-if (p_v[id].size() == 0)
-{
-// 모든 above에서 그 숫자 제거
-   for(int i=0; i<p_v.size(); i++)
-   {
-    p_v[i].above.erase(std::remove(p_v[i].above.begin(), p_v[i].above.end(), id), p_v[i].above.end());
-    add_board();
-    make_l.push_back(p_v[id]);
-    print_board();
-   }
+    if (p_v[id].above.size() == 0)
+    {
+        // 모든 above에서 그 숫자 제거
+        for (int i = 0; i < p_v.size(); i++)
+        {
+            p_v[i].above.erase(std::remove(p_v[i].above.begin(), p_v[i].above.end(), id), p_v[i].above.end());
+            add_board();
+            make_l.push_back(p_v[id]);
+            print_board();
+        }
+    }
+    else {
+        for (int i = 0; i < p_v[id].above.size(); i++)
+        {
+            delete_board(p_v[i].getid());
+        }
+    }
 }
-else{
-for(int i=0; i<p_v[id].above.size(); i++)
-{
-   delete_board(p_v[i].getid());
-}
-}
-}
-
 
--- Project1/page.h
+++ Project1/page.h
@@ -3,26 +3,25 @@
 
 class Page {
 public:
-    int getX() { return x;}
+    std::vector<int> above;
+    int getX() { return x; }
     int getY() { return y; }
     int getW() { return width; }
     int getH() { return height; }
     int getid() { return id; }
     char getcont() { return content; }
-    vector<int>& get_vec() { return above; }
-    void set_vec(int a) { above.push_back(a); }
-    void asd_vec() { sort(above.begin(), above.end()); }//오름차순
-    void des_vec() { reverse(above.begin(), above.end()); }//내림차순
-    vector <int> above;
-    vector <int> make_l;
+    //void asd_vec() { sort(above.begin(), above.end()); }//오름차순
+    //void des_vec() { reverse(above.begin(), above.end()); }//내림차순
+    
     Page(int x, int y, int width, int height, int id, int content)
         : x(x), y(y), width(width), height(height), id(id), content(content) {}
-
-
-private:
-    
     int x, y; // position of the page on the board
     int width, height; // width and height of the page 
     int id; // unique id for each page
     char content;
+
+
+private:
+
+
 };
