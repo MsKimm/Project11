72f6906c9e07412d661ecb54750e86858b23b733
MsKim <myclath@snu.ac.kr>
2023-05-07 19:29:57 +0900

    almost..

--- Project1/board.h
+++ Project1/board.h
@@ -19,6 +19,9 @@ class Board {
         void modify_content(int id, char content);
         void modify_position(int id, int x, int y);
 
+        //made functions
+        void Board::add_board();
+        void Board::delete_board(int id);
     private:
         vector <Page> p_v;
         int num_jobs, width, height; 
@@ -113,96 +116,100 @@ void Board::insert_page(int x, int y, int width, int height, int id, int content
     print_board();
 }
 
-void Board::delete_page(int id) {   //8
-
-    vector <int> temp_vec;
- 
-    for (int i = 0; i < p_v.size(); i++) {
-        if (p_v[i].getid() == id) {
-            temp_vec = p_v[i].get_vec();   // 그 페이지의 above 를 가져옴
+void Board::delete_page(int id) {
+    delete_board(id);
+    for(int i=0; i<p_v.size(); i++) //p_v에서 지운 아이디를 삭제한다.
+    {
+        if(p_v[i].id == id)
+        {
+            p_v.erase(p_v.begin() + i);
         }
+        break;
     }
-    if (temp_vec.empty()) {
-        //보드에서 해당 페이지를 지워준다.
-        
+    make_l.pop_back(); //맨뒤의 id는 다시 만들게 아니므로 제거
+    for(int i=make_l.size()-1; i>=0; i--)
+    {
+        insert_page(make_l[i].x, make_l[i].y, make_l[i].width, make_l[i].height, make_l[i].id, make_l[i].content);
+        make_l.pop_back();
     }
-    for (int i = 0; i < temp_vec.size(); i++) {
-        delete_page(temp_vec[i]);
-    }
-    
-    //delete_board(id);
-    //for (i=make_l-2; i>=0; i++)
-    //{
-    //    insert_page(make_l[i]);
-    //}
-    
-
 }
 
 void Board::modify_content(int id, char content) {
-Page p(int x,int y,int width,int height,int id, content);
-for(int i=0; i<p_v.size(); i++ )
-{
-    if(p_v[i].getid() == id)
+    int target;
+    delete_board(id);
+    for(int i=0; i<p_v.size(); i++) //p_v에서 지운 아이디를 삭제한다.
     {
-        p = p_v[i];
+        if(p_v[i].id == id)
+        {
+            target = i;
+            p_v[i].content = content;
+        }
+        break;
     }
-}
-
-//delete_board(id);
-//insert_page(p.getX(), p.getY(), p.getW(), p.getH(), p.getid, content)
-    //for (i=make_l-2; i>=0; i++)
-    //{
-    //    insert_page(make_l[i]);
-    //}
+    insert_page(p_v[target].x, p_v[target].y, p_v[target].width, p_v[target].height, p_v[target].id, p_v[target].content);
+    make_l.pop_back(); //맨뒤의 id는 다시 만들게 아니므로 제거
+    for(int i=make_l.size()-1; i>=0; i--)
+    {
+        insert_page(make_l[i].x, make_l[i].y, make_l[i].width, make_l[i].height, make_l[i].id, make_l[i].content);
+        make_l.pop_back();
+    }
 }
 void Board::modify_position(int id, int x, int y) {
-   Page p(x, y, width, height, id, content);
-for(i=0; i<p_v.size(); i++ )
-{
-    if(p_v[i].getid() == id)
+    int target;
+    delete_board(id);
+    for(int i=0; i<p_v.size(); i++) //p_v에서 지운 아이디를 삭제한다.
     {
-        p = p_v[i];
+        if(p_v[i].id == id)
+        {
+            target = i;
+            p_v[i].x = x;
+            p_v[i].y = y;
+        }
+        break;
+    }
+    insert_page(p_v[target].x, p_v[target].y, p_v[target].width, p_v[target].height, p_v[target].id, p_v[target].content);
+    make_l.pop_back(); //맨뒤의 id는 다시 만들게 아니므로 제거
+    for(int i=make_l.size()-1; i>=0; i--)
+    {
+        insert_page(make_l[i].x, make_l[i].y, make_l[i].width, make_l[i].height, make_l[i].id, make_l[i].content);
+        make_l.pop_back();
     }
 }
-    //delete_board
-    //insert_page(x,y, p.getW(), p.getH(), p.getid, p.getcont)
-    //for (i=make_l-2; i>=0; i++)
-    //{
-    //    insert_page(make_l[i]);
-    //}
+
+void Board::add_board()
+{
+    for(int i=0; i<p_v.size(); i++)
+    {
+        for (int j = p_v[i].y; j < p_v[i].y+height; j++) {
+            for (int k = p_v[i].x; k < p_v[i].x+width; k++) 
+            {
+                board[width * p_v[i].y + p_v[i].x] = p_v[i].content;
+            }
+            
+        }
+    }
 }
 
-//void Board::add_board(i)
-//if (p_v[id].size() == 0)
-//{
-// for (int j = y; j < y+height; j++) {
-//          for (int k = x; k < x+width; k++) {
-//             board[width * y + x] = p.content;
-//     }
-//}
-//}
-//for(int i=0; i<p_v[id]; i++)
-//{
-//add_board(i);
-//}
 
-// 또는 print_above : 모든 벡터 출력(above 와 함깨)
-
-//void Board::delete_board(int id)
-//{
-//vector make_l; 순서 기록할 벡터
-//if (p_v[id].size() == 0)
-//{
-//    above에서 그 숫자 제거
-//    add_board() - 전체
-//    make_l.push_back(id);
-//    print_board();
-//}
-//for(i=p_v[id].size()-1; i>=0; i--)
-//{
-//    delete_board(p_v[i].getid());
-//}
-//}
+void Board::delete_board(int id)
+{
+if (p_v[id].size() == 0)
+{
+// 모든 above에서 그 숫자 제거
+   for(int i=0; i<p_v.size(); i++)
+   {
+    p_v[i].above.erase(std::remove(p_v[i].above.begin(), p_v[i].above.end(), id), p_v[i].above.end());
+    add_board();
+    make_l.push_back(p_v[id]);
+    print_board();
+   }
+}
+else{
+for(int i=0; i<p_v[id].above.size(); i++)
+{
+   delete_board(p_v[i].getid());
+}
+}
+}
 
 
